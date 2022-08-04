3b985a28b63853b1b021cbc79f8bc8c3cc06fdbb
Gustavo Henrique <gustavohenrique10002@gmail.com>
2022-08-03 21:51:14 -0300

    Concluido o inicio do jogo de carro!

---
+++ MotordeLamborguini.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ProjetoGradleJava;
+
+/**
+ *
+ * @author Admin
+ */
+public class MotordeLamborguini implements IMotor {
+
+    @Override
+    public String acelerar(){
+    return "VVRRRUMMMMMM!!!!!!(lamborguini)";
+    }
+    
+    
+    
+}
